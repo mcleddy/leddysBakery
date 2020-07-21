683a868ec6e14ed45f0269e7b7e4ac7e79aece53
Mary Leddy <maryleddy1@gmail.com>
2020-07-21 15:30:11 -0700

    m

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require('express');
-const app = express();
+// Dependencies
+const express = require('express')
+const app = express()
 
-// Serve all the files in '/dist' directory
-app.use(express.static('dist'));
+// Port
+// Allow use of Heroku's port or your own local port, depending on your environment
+const PORT = process.env.PORT || 3000
 
+// use public folder for static assets, like css
+app.use(express.static('dist'))
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+// Listen
+app.listen(PORT, ()=>{
+  console.log('Listening on port: ', PORT  );
+})
